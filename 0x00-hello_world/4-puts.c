0d18d7593689449beb59a912a07b7d757513b11d
Magokong97 <magokong97@gmail.com>
2023-06-15 16:49:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 /**
  * main - 4-puts.c
  * description - Write a C program that ptints exctly Programming is like building a multilingual puzzle
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
