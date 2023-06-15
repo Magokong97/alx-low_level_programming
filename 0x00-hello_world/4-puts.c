5cf6a4f3dea6a6a13e6240101611500771f1e2f1
Magokong97 <magokong97@gmail.com>
2023-06-15 16:54:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - 4-puts.c
- * description - Write a C program that ptints exctly Programming is like building a multilingual puzzle
- * 
+ * description - Write a C program that prints exctly
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
