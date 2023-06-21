25f51713a61149c548f2532f4b9e36ef49b50e5b
Magokong97 <magokong97@gmail.com>
2023-06-21 14:05:42 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - rints the sign of a number.
+ * @c: The number to be chacked
+ * Return: 1 for positive number, -1 for negetive num or 0 for anything esle.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
