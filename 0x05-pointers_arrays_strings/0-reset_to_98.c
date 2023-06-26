9911aefbc02b9a321e626f9f35d08c3e48974c89
Magokong97 <magokong97@gmail.com>
2023-06-26 14:15:47 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_t0_98 - takes a pointer to an int as parameter
+ * and dates the value it points to to 98
+ * @n: input
+ * Return: n
+ */
+
+void rest_to_98(int *n)
+{
+	*n - 98;
+}
